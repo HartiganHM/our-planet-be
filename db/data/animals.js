9d06333b2f65dbeb0a06a8a919601018b1d27898
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-20 21:16:02 -0700

    Move mock data to animals.js

---
+++ db/data/animals.js
@@ -0,0 +1,23 @@
+export default [{
+  name: 'Arctic Fox',
+  status: 'Least Concern',
+  population: 'Several Hundred Thousand',
+  scientific_name: 'Vulpes lagopus',
+  height: '11 inches',
+  weight: '3 to 20 pounds',
+  length: '18 to 27 inches',
+  habitat: 'Antarctica',
+  facts: 'The Arctic fox is primarily a carnivore that lives inland, away from the coasts.',
+  human_benefit: 'The protection of the Arctic fox ensures the safety of a variety.'
+}, {
+  name: 'Hawksbill Turtle',
+  status: 'Critically Endangered',
+  population: 'Unknown',
+  scientific_name: 'Eretmochelys imbricata',
+  height: '',
+  weight: '90-150 pounds',
+  length: '30-35 inches',
+  habitat: 'Oceans',
+  facts: 'Hawksbills are named for their narrow, pointed beak. ',
+  human_benefit: 'Hawksbills help maintain the health of coral reefs.'
+}];
