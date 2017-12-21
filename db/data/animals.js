571dbce02b46e6d57e54b4ba2aea7689a5c75cde
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-20 21:30:49 -0700

    Extend animal facts and module exports variables

--- db/data/animals.js
+++ db/data/animals.js
@@ -1,4 +1,4 @@
-export default [{
+const animalsData = [{
   name: 'Arctic Fox',
   status: 'Least Concern',
   population: 'Several Hundred Thousand',
@@ -7,8 +7,8 @@ export default [{
   weight: '3 to 20 pounds',
   length: '18 to 27 inches',
   habitat: 'Antarctica',
-  facts: 'The Arctic fox is primarily a carnivore that lives inland, away from the coasts.',
-  human_benefit: 'The protection of the Arctic fox ensures the safety of a variety.'
+  facts: 'The Arctic fox is primarily a carnivore that lives inland, away from the coasts. They are dependent on the presence of smaller animals (most often lemmings) to survive.  Arctic foxes also hunt for sea birds, fish, and other marine life. Smaller rodent populations waver between times of abundance and scarcity, which leaves the Arctic fox vulnerable when these creatures are low in numbers.',
+  human_benefit: 'The protection of the Arctic fox ensures the safety of a variety of other wildlife in the Arctic region.'
 }, {
   name: 'Hawksbill Turtle',
   status: 'Critically Endangered',
@@ -18,6 +18,8 @@ export default [{
   weight: '90-150 pounds',
   length: '30-35 inches',
   habitat: 'Oceans',
-  facts: 'Hawksbills are named for their narrow, pointed beak. ',
-  human_benefit: 'Hawksbills help maintain the health of coral reefs.'
+  facts: 'Hawksbills are named for their narrow, pointed beak. They also have a distinctive pattern of overlapping scales on their shells that form a serrated-look on the edges. These colored and patterned shells make them highly-valuable and commonly sold as "tortoiseshell" in markets. Hawksbills are found mainly throughout the world\'s tropical oceans, predominantly in coral reefs. They feed mainly on sponges by using their narrow pointed beaks to extract them from crevices on the reef, but also eat sea anemones and jellyfish. Sea turtles are the living representatives of a group of reptiles that has existed on Earth and travelled our seas for the last 100 million years. They are a fundamental link in marine ecosystems and help maintain the health of coral reefs and sea grass beds.',
+  human_benefit: 'Hawksbills are named for their narrow, pointed beak. They also have a distinctive pattern of overlapping scales on their shells that form a serrated-look on the edges. These colored and patterned shells make them highly-valuable and commonly sold as "tortoiseshell" in markets. Hawksbills are found mainly throughout the world\'s tropical oceans, predominantly in coral reefs. They feed mainly on sponges by using their narrow pointed beaks to extract them from crevices on the reef, but also eat sea anemones and jellyfish. Sea turtles are the living representatives of a group of reptiles that has existed on Earth and travelled our seas for the last 100 million years. They are a fundamental link in marine ecosystems and help maintain the health of coral reefs and sea grass beds.'
 }];
+
+module.exports = animalsData;
--- db/data/continents.js
+++ db/data/continents.js
@@ -1,7 +1,9 @@
-export default [{
+const continentsData = [{
   name: 'Antarctica',
   bio: 'Social enterprise, empower communities a segmentation best practices issue outcomes co-creation. Venture philanthropy; movements we must stand up; inspire academic because entrepreneur shared value movements. Co-create outcomes low-hanging fruit philanthropy; B-corp impact; think tank ideate. Revolutionary, because think tank impact investing storytelling corporate social responsibility. Her body her rights because.'
 },{
   name: 'Oceans',
   bio: 'Social enterprise, empower communities a segmentation best practices issue outcomes co-creation. Venture philanthropy; movements we must stand up; inspire academic because entrepreneur shared value movements. Co-create outcomes low-hanging fruit philanthropy; B-corp impact; think tank ideate. Revolutionary, because think tank impact investing storytelling corporate social responsibility. Her body her rights because.'
 }];
+
+module.exports = continentsData;
