dc83f5c414ab4ccefa1e9db89b2fd8880fa8bded
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-20 21:14:54 -0700

    Create data directory and animals/continents files

--- db/seeds/dev/continents-animals-initial.js
+++ db/seeds/dev/continents-animals-initial.js
@@ -1,10 +1,4 @@
-let continentsData = [{
-  name: 'Antarctica',
-  bio: 'Social enterprise, empower communities a segmentation best practices issue outcomes co-creation. Venture philanthropy; movements we must stand up; inspire academic because entrepreneur shared value movements. Co-create outcomes low-hanging fruit philanthropy; B-corp impact; think tank ideate. Revolutionary, because think tank impact investing storytelling corporate social responsibility. Her body her rights because.'
-},{
-  name: 'Oceans',
-  bio: 'Social enterprise, empower communities a segmentation best practices issue outcomes co-creation. Venture philanthropy; movements we must stand up; inspire academic because entrepreneur shared value movements. Co-create outcomes low-hanging fruit philanthropy; B-corp impact; think tank ideate. Revolutionary, because think tank impact investing storytelling corporate social responsibility. Her body her rights because.'
-}];
+
 
 let animalsData = [{
   name: 'Arctic Fox',
