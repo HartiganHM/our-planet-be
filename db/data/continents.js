44e5ecf41badb2a456c6697391851f93eeed1255
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-20 22:31:41 -0700

    Chance to Antarctica / Artic

--- db/data/continents.js
+++ db/data/continents.js
@@ -2,7 +2,7 @@ const continentsData = [{
   name: 'Africa',
   bio: 'Africa straddles the equator which bestows a rich diversity of wildlife and landscapes on the world\'s second largest continent. Large predators roam the sweeping savannas among huge herds of grazing herbivores, while apes, monkeys and snakes inhabit its dark, dense forests. Widely regarded as the place where human life originated, Africa is home to many of the world\'s best loved and most fascinating animals, as well as to some of its most endangered. Yet it\'s also home to many species that are as familiar to us in the UK as they are to Africans.'
 }, {
-  name: 'Antarctica',
+  name: 'Antarctica / Arctic',
   bio: 'Antarctica holds the record for being the coldest place on Earth, which together with extensive ice cover and lack of winter sunlight makes the continent pretty inhospitable to life. The sea, rather than the land, is what supports most creatures such as penguins and seals. It\'s often warmer than the land, so is a good place to retreat to and warm up to a balmy -2 Celsius. Permanently land-living plants and animals in Antarctica are small and ground-hugging to avoid wind chill and the accompanying dehydration.'
 }, {
   name: 'Asia',
