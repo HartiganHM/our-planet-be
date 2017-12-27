014a3211a7e704ab54e0e802dfa049bbdbce9fe1
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 06:59:04 -0700

    Fix typo

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const timeLogger = (request, response, next) => {
   next();
 };
 
-const accessControlAllowOrigin = (request, repsonse, next) => {
+const accessControlAllowOrigin = (request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*');
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
