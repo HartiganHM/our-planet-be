541b407f5421c2ddf8295109e8fb578489b0c5bb
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-11 16:44:45 -0600

    featL Write animals by continent_id endpoint

--- server.js
+++ server.js
@@ -27,25 +27,38 @@ const accessControlAllowOrigin = (request, response, next) => {
 
 const server = app
   .use(accessControlAllowOrigin, urlLogger, timeLogger)
+  // Continents endpoints
   .get('/api/v1/continents', (request, response) => {
     database('continents')
       .select()
-      .then(continents => {
+      .then((continents) => {
         response.status(200).json(continents);
       })
-      .catch(error => {
+      .catch((error) => {
         response.status(500).json({ error });
       });
   })
+  // Animals endpoints
+  // GET all animals
   .get('/api/v1/animals', (request, response) => {
     database('animals')
       .select()
-      .then(animals => {
+      .then((animals) => {
         response.status(200).json(animals);
       })
-      .catch(error => {
+      .catch((error) => {
         response.status(500).json({ error });
       });
+  })
+  // GET animals by continent_id
+  .get(`/api/v1/animals/:continent_id`, (request, response) => {
+    const { continent_id } = request.params;
+
+    database('animals')
+      .where('continent_id', continent_id)
+      .select()
+      .then((animals) => response.status(200).json(animals))
+      .catch((error) => response.status(500).json({ error }));
   })
   .listen(port, () => {
     console.log('Our Planet BE running on port ' + port);
