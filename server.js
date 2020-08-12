6ccdb800d3f61b58c684a5f29a110c66dedc34c8
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-12 05:27:39 -0600

    chore: Build handleReturnAnimalById helper; refactor message formatting

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 const port = process.env.PORT || 3001;
 
+const EXCLUDED_PROPERTIES = ['id', 'continent_id', 'created_at', 'updated_at'];
+const ANIMAL_COLUMN_ENUM = {
+  name: { label: 'Name', order: 0 },
+  scientific_name: { label: 'Scientific Name', order: 1 },
+  status: { label: 'Endangered Status', order: 2 },
+  habitat: { label: 'Habitat', order: 3 },
+  population: { label: 'Population', order: 4 },
+  height: { label: 'Height', order: 5 },
+  weight: { label: 'Weight', order: 6 },
+  length: { label: 'Length', order: 7 },
+  facts: { label: 'Facts', order: 8 },
+  human_benefit: { label: 'How they help us', order: 9 },
+};
 const DEFAULT_ANIMAL_RESPONSES = {
   countryId: '',
   animalId: '',
@@ -38,18 +51,17 @@ const handleInitialMessage = async (request, response, twiml) => {
   request.session.animalResponses = DEFAULT_ANIMAL_RESPONSES;
 
   const continents = await database('continents').select();
+  const continentsList = continents.reduce(
+    (accumulator, { id, name }) => (accumulator += `\t${id} - ${name}\n`),
+    ''
+  );
 
   twiml.message(`
     Thanks for writing to Our Planet! 🌎
 
     We aim to shed light on endangered animals using knowledge from the World Wildlife Foundation alongside beautiful photos. Who knows, maybe you'll find your new spirit animal! ✨
 
-    Let's start by selecting a continent. Reply with a number and we'll send you a list of endangered animals on that continent:
-
-    ${continents.reduce(
-      (accumulator, { id, name }) => (accumulator += `\t ${id} - ${name}\n`),
-      ''
-    )}
+    Let's start by selecting a continent. Reply with a number and we'll send you a list of endangered animals on that continent:\n\n${continentsList}
   `);
 
   response.writeHead(200, { 'Content-Type': 'text/xml' });
@@ -74,17 +86,61 @@ const handleReturnAnimals = async (request, response, twiml) => {
     .where('continent_id', countryId)
     .select();
 
+  const animalsList = animalsByContinent
+    .sort((animalA, animalB) => (animalA.id > animalB.id ? 1 : -1))
+    .reduce(
+      (accumulator, { id, name }) => (accumulator += `\t${id} - ${name}\n`),
+      ''
+    );
+
   twiml.message(`
     Sure, here are some animals that are endangered in ${continent[0].name}.
 
-    Send me the animal's number and I'll send you some facts about that animal!
+    Send me the animal's number and I'll send you some facts about that animal!\n\n${animalsList}
+  `);
 
-    ${animalsByContinent
-      .sort((animalA, animalB) => (animalA.id > animalB.id ? 1 : -1))
-      .reduce(
-        (accumulator, { id, name }) => (accumulator += `\t ${id} - ${name}\n`),
-        ''
-      )}
+  response.writeHead(200, { 'Content-Type': 'text/xml' });
+  response.end(twiml.toString());
+};
+
+const handleReturnAnimalById = async (request, response, twiml) => {
+  const {
+    session,
+    body: { Body: animalId },
+  } = request;
+
+  request.session.animalResponses = {
+    ...session.animalResponses,
+    animalId,
+  };
+
+  const animal = await database('animals').where('id', animalId).select();
+  const existingAnimalProperties = Object.keys(animal[0])
+    .filter(
+      (property) =>
+        !EXCLUDED_PROPERTIES.includes(property) && animal[0][property]
+    )
+    .sort((propertyA, propertyB) =>
+      ANIMAL_COLUMN_ENUM[propertyA].order > ANIMAL_COLUMN_ENUM[propertyB].order
+        ? 1
+        : -1
+    )
+    .reduce(
+      (accumulator, property) => ({
+        ...accumulator,
+        [ANIMAL_COLUMN_ENUM[property].label]: animal[0][property],
+      }),
+      {}
+    );
+
+  const animalPropertiesList = Object.keys(existingAnimalProperties).reduce(
+    (accumulator, property) =>
+      (accumulator += `${property}:\n\t${existingAnimalProperties[property]}\n\n`),
+    ''
+  );
+
+  twiml.message(`
+    Here are all of the facts we have on the ${existingAnimalProperties.Name}!\n\n${animalPropertiesList}
   `);
 
   response.writeHead(200, { 'Content-Type': 'text/xml' });
@@ -142,9 +198,10 @@ const server = app
       return;
     }
 
-    // if (!animalId) {
-    //   handleReturnAnimalById(request, response, twiml);
-    // }
+    if (!animalId) {
+      handleReturnAnimalById(request, response, twiml);
+      return;
+    }
   })
   .listen(port, () => {
     console.log('Our Planet BE running on port ' + port);
