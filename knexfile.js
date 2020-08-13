d90bc66e7fcbc349f2504a1719d0de19455bf4b7
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-13 08:31:16 -0600

    chore: Update knexfile with rejectUnauthorized

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
+    ssl: {
+      rejectUnauthorized: false
+    },
     migrations: {
       directory: './db/migrations'
     },
